ed94ddda533e6dc023d15af43bcfc7f6f8ffda5a
halid vejzovic <halidvejzovic@outlook.com>
2026-09-24 03:42:41 +0200

    10:V - Börrjade med JS QuerySelector-form

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let formRef;
+
+function init() {
+    formRef = document.querySelector("form");
+    console.log(formRef);
+    
+    
+}
+
+window.onload = init;
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* DOM = Document Object Model
+
+DOM-manipulation handlar om att:
+
+Hämta element (getElementById, querySelector)
+Ändra innehåll (textContent, innerHTML)
+Ändra stil (style)
+Lägga till eller ta bort element (createElement, appendChild, remove)
+Hantera händelser (addEventListener)
+Ändra attribut (setAttribute, getAttribute) 
+*/
